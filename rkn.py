aa0bea94d030b2dbfcae94738922edddd14599ab
R. Serge <mail@r-serge.info>
2017-05-26 17:50:44 +1000

    Added full logging support

--- rkn.py
+++ rkn.py
@@ -5,14 +5,16 @@ import logging
 import time
 
 # Test URL
-# URL = 'http://vigruzki.rkn.gov.ru/services/OperatorRequestTest/?wsdl'
-URL = 'http://vigruzki.rkn.gov.ru/services/OperatorRequest/?wsdl'
+URL = 'http://vigruzki.rkn.gov.ru/services/OperatorRequestTest/?wsdl'
+# URL = 'http://vigruzki.rkn.gov.ru/services/OperatorRequest/?wsdl'
 # Request format
 DUMPVER = '2.2'
 # Max number of attempts to downloading file
 MAXTRIES = 5
 # Log format
-LOGFORMAT = '%(asctime)s %(levelname)s %(message)s'
+LOGFORMAT = '%(asctime)s %(levelname)s Line:%(lineno)d %(message)s'
+# Logging levels
+LEVELS = ("INFO", "ERROR")
 
 
 def sendQuery(wsdlClient, queryFile, queryFileSing):
@@ -20,6 +22,7 @@ def sendQuery(wsdlClient, queryFile, queryFileSing):
     Send a request to rkn
     Returns code string or None if error
     '''
+    logger = logging.getLogger(__name__)
     request = b64encode(queryFile.read()).decode()
     request_sign = b64encode(queryFileSing.read()).decode()
 
@@ -28,14 +31,14 @@ def sendQuery(wsdlClient, queryFile, queryFileSing):
                                                 signatureFile=request_sign,
                                                 dumpFormatVersion=DUMPVER)
     except Exception as e:
-        print('Error send request', e)
+        logger.error('Error send request' + str(e))
         return None
 
     if result['result'] == True:
-        print(result['code'])
+        logger.info("Result successfull. Result code: " + result['code'])
         return(result['code'])
     else:
-        print(result['resultComment'])
+        logger.error("Error: " + result['resultComment'])
         return None
 
 
@@ -45,30 +48,31 @@ def getFile(wsdlClient, codeString):
     Returns archive or None if error
     '''
     tries = 0
+    logger = logging.getLogger(__name__)
 
     try:
         while tries < MAXTRIES:
             result = wsdlClient.service.getResult(code=codeString)
 
             if result['resultCode'] == 0:
-                print('Wait 60 seconds: ', result['resultComment'])
+                logger.info('Wait 60 seconds: ' + result['resultComment'])
                 time.sleep(60)
                 tries += 1
                 continue
 
             elif result['resultCode'] == 1:
-                print('Download success')
+                logger.info('Download success')
                 return result['registerZipArchive']
 
             else:
-                print("Error: ", result['resultComment'])
+                logger.error("Error: " + result['resultComment'])
                 return None
 
-        print("All tries exceeded")
+        logger.error("All tries exceeded")
         return None
 
     except Exception as e:
-        print('Some exception ', e)
+        logger.error('Some exception: ' + str(e))
         return None
 
 
@@ -82,9 +86,24 @@ def addArgs():
                         help="Path to query file")
     parser.add_argument("querySign",
                         type=FileType('rb'),
-                        help="Query file signed with EDP")
+                        help="Query file signed with DS")
     parser.add_argument("output",
                         help="File to save information from RKN")
+    parser.add_argument("-l",
+                        "--logfile",
+                        help="File for write logs.",
+                        default="rkn.log")
+    parser.add_argument("-p",
+                        "--print",
+                        action='store_true',
+                        help="Print logginig messages only to console")
+    parser.add_argument("-s",
+                        "--severity",
+                        help="""
+                        Logging severity.
+                        Might be INFO or ERROR
+                        """,
+                        default="INFO")
     args = parser.parse_args()
     return args
 
@@ -93,15 +112,41 @@ def main():
     '''
     Start point
     '''
+    # Parse args
     args = addArgs()
-    formatter = logging.Formatter(LOGFORMAT)
-    logging.basicConfig(format=formatter, level=logging.DEBUG)
 
+    if str.upper(args.severity) in LEVELS:
+        level = str.upper(args.severity)
+    else:
+        level = "INFO"
+
+    # Init logger
+    logger = logging.getLogger(__name__)
+    logger.setLevel(level)
+
+    formatter = logging.Formatter(LOGFORMAT)
+
+    logFile = logging.FileHandler(args.logfile)
+    logFile.setFormatter(formatter)
+    logFile.setLevel(level)
+
+    if args.print:
+        console = logging.StreamHandler()
+        console.setFormatter(formatter)
+        console.setLevel(level)
+
+        logger.addHandler(console)
+    else:
+        logger.addHandler(logFile)
+
+    logger.info("Script started")
+
+    # Get url and start processing
     try:
         client = Client(URL, cache=None)
 
     except Exception as e:
-        print("Error init the client: ", e)
+        logger.error("Error init the client: " + str(e))
         exit(1)
 
     code = sendQuery(client, args.query, args.querySign)
@@ -110,18 +155,19 @@ def main():
         zipfile = getFile(client, code)
 
         if zipfile:
-            print("Writing file ", args.output)
+            logger.info("Writing file " + str(args.output))
 
             try:
                 with open(args.output, 'wb') as file:
                     content = b64decode(zipfile.encode())
-                    bytes = file.write(content)
-                    print("File {} was writed. Size: {} KB".format(args.output, round(bytes/1048, 2)))
+                    bytes = round(file.write(content)/1024, 2)
+                    logger.info("File {} was writed. Size: {} KB".format(args.output, bytes))
             except Exception as e:
-                print("Error writing file", e)
+                logger.error("Error writing file: " + str(e))
 
     args.query.close()
     args.querySign.close()
+    logger.info("Script finished")
     exit(0)
 
 

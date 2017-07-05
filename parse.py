db730877851b12f4ca961ceb5efd1a116820d85f
Serge <you@example.com>
2017-07-05 13:39:02 +1000

    Worked version 0.1

--- parse.py
+++ parse.py
@@ -1,12 +1,12 @@
 from lxml import etree
 from urllib.parse import urlparse
 
-
 FILE_NAME = 'dump.xml'
+WHITELIST = 'whitelist.txt'
+
 
 def parse(dump):
 	records = []
-
 	blocked_ips = []
 	blocked_subnets = []
 	blocked_domains = []
@@ -15,8 +15,8 @@ def parse(dump):
 	parser = etree.parse(dump)
 	root = parser.getroot()
 
+	# Govnocode start
 	for node in root:
-
 		records.append({  'urls':		[item.text for item in node if item.tag == 'url'], 
 						  'domains': 	[item.text for item in node if item.tag == 'domain'],
 						  'ips': 		[item.text for item in node if item.tag == 'ip'],
@@ -24,30 +24,67 @@ def parse(dump):
 						  })
 
 	for record in records:
-
-		#print("Value is: {}".format(record))
+		# Determine subnets
 		for subnet in record['subnets']:
 			blocked_subnets.append(subnet)
 
+		# Determine domains - to compare it with url
+		for domain in record['domains']:
+			temp_domains = []
+			if record['urls'] == []:
+				domain = domain.encode('idna')
+				temp_domains.append(domain.decode())
+
+		# Determine urls and encode to puhhycode
 		for url in record['urls']:
 			res = urlparse(url)
-			if res.scheme == 'https' or (record['urls'] == [] and record['domains'] == []):
-				# print("Domain {} will blocked by IPs {}".format(res.hostname, record['ips']))
-				blocked_ips.append(record['ips'])
-			elif res.scheme == 'http':
-				blocked_urls.append(url)
-				
+			if res.scheme == 'https':
+				if record['ips'] == []:
+					print("Domain {} is have not IP =(".format(url))
+				for ip in record['ips']:
+					blocked_ips.append(ip)
 
+			elif res.scheme == 'http':
+				domain = res.netloc.encode('idna')
+				url = domain.decode() + ':' + res.path + '*'
+				blocked_urls.append(url)
+
+			# Determine domains and compare it with url
+			if domain in temp_domains:
+				temp_domains.remove(domain)
+
+		# Fill blocked domain list
+		blocked_domains += temp_domains
+
+		# Determine ips
+		for ip in record['ips']:
+			if record['urls'] == [] and record['domains'] == []:
+				blocked_ips.append(ip)
+
+	# Only uniq items
 	subnets_set = set(blocked_subnets)
-	ip_set = set(sum(blocked_ips,[]))
+	ip_set = set(blocked_ips)
 	url_set = set(blocked_urls)
+	domains_set = set(blocked_domains)
 
 	print("Count all subnets: {}".format(len(subnets_set)))
 	print("Count all ip's: {}".format(len(ip_set)))
 	print("Count all urls: {}".format(len(url_set)))
-
+	print("Count all domains: {}".format(len(domains_set)))
 	print("Count of all records: {}".format(len(root)))
 
+	with open("result.txt", 'w') as fd:
+	 	fd.write("*:")
+
+	 	# Write domains
+	 	for domain in domains_set:
+	 		fd.write(domain + ":*\n")
+
+	 	for ip in ip_set:
+	 		fd.write(ip + ":*\n")
+
+	 	for url in url_set:
+	 		fd.write(url + "\n")
 
 if __name__ == '__main__':
 	parse(dump=FILE_NAME)
